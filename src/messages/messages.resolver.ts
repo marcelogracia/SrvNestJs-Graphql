0df06e0097e6db5f1f0676baf62be92872d793b3
Marcelo Graca <marcelogracia@hotmail.com>
2019-10-15 22:57:46 -0300

    inicio2

---
+++ src/messages/messages.resolver.ts
@@ -0,0 +1,25 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+
+@Resolver()
+export class MessagesResolver {
+  // this is just for demonstration purposes
+  // do NOT do this in real-life
+  // this is meant as a substitute for a databse
+  messagesThatReallyShouldBeInADb = [
+    { id: 0, description: 'The seed message' },
+  ];
+
+  @Query()
+  messages() {
+    return this.messagesThatReallyShouldBeInADb;
+  }
+  
+  @Mutation()
+  createMessage(@Args('description') description: string) {
+    const id = this.messagesThatReallyShouldBeInADb.length;
+    const newMessage = { id, description };
+    this.messagesThatReallyShouldBeInADb.push(newMessage);
+    return newMessage;
+  }
+
+}
